b6b80a58c3d5a7c571eee768eb76d47491fea4fa
archetype <archetype93@gmail.com>
2015-06-22 02:41:53 +0900

    Population should do flow control
    and this is being so weird..

--- GA_Practice_Devil'sCamp2015/src/algorithm/Population.java
+++ GA_Practice_Devil'sCamp2015/src/algorithm/Population.java
@@ -1,5 +1,23 @@
 package algorithm;
 
-public class Population {
+import java.util.ArrayList;
 
+public class Population {
+	private ArrayList<Individual> generation;
+	private Individual noSelection;
+	private int maxGen=100;
+	private final int maxPopulation=60;
+	
+	
+	private data.Division test = data.Storage.getStorageInstance().getTest();
+	
+	public Population(int max) {
+		this.maxGen = max;
+	}
+	
+	public void run() {
+		for(int i=0;i<this.maxGen;i++) {
+			
+		}
+	}
 }
