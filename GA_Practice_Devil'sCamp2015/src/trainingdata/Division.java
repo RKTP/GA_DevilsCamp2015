a88b0283b283115bdc8a77042648314ade91aca1
archetype <archetype93@gmail.com>
2015-06-22 01:11:37 +0900

    FileIO building
    ,,

--- GA_Practice_Devil'sCamp2015/src/trainingdata/Division.java
+++ GA_Practice_Devil'sCamp2015/src/trainingdata/Division.java
@@ -1,5 +1,24 @@
 package trainingdata;
 
-public class Division {
+import java.util.*;
 
+public class Division {
+	ArrayList<Double> attribute;
+	ArrayList<Boolean> label;
+	
+	public ArrayList<Double> getAtt() {
+		return this.attribute;
+	}
+	
+	public ArrayList<Boolean> getLabel() {
+		return this.label;
+	}
+	
+	public void setAtt(ArrayList<Double> att) {
+		this.attribute = att;
+	}
+	
+	public void setLabel(ArrayList<Boolean> label) {
+		this.label = label;
+	}
 }
--- GA_Practice_Devil'sCamp2015/src/trainingdata/Storage.java
+++ GA_Practice_Devil'sCamp2015/src/trainingdata/Storage.java
@@ -1,5 +1,14 @@
 package trainingdata;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class Storage {
+	File trainFile = new File("trainData");
+	File testFile = new File("testData");
+	InputStream fileStream = InputStream.getInstance();
+	
+	Division trainSet = fileStream.setData(trainFile);
+	Division testSet =fileStream.setData(testFile);
 
 }
