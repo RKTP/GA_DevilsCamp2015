c080d5654611eb40f031e9fb9b61eaf0f05883e7
archetype <archetype93@gmail.com>
2015-06-23 00:16:34 +0900

    algorithm basis implement done + sample added
    data management is fully implemented

--- GA_Practice_Devil'sCamp2015/src/run/GeneticAlgorithm.java
+++ GA_Practice_Devil'sCamp2015/src/run/GeneticAlgorithm.java
@@ -3,6 +3,7 @@ package run;
 public class GeneticAlgorithm {
 
 	public static void main(String[] args) {
-
+		algorithm.Population geneticAlgorithm = new algorithm.Population(100);
+		geneticAlgorithm.run();
 	}
 }
